8629f3fe381178656045e9626342445867d038b2
nikshadali <nikali92100@gmail.com>
2020-06-21 03:54:29 -0700

    Upload assignment

---
+++ aap.js
@@ -0,0 +1,365 @@
+//  Assignment # 6
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 1 *****************************
+
+// var fname = prompt("Enter your first Name")
+// var lname = prompt("Enter your Last Name")
+
+// var fullname = fname + " " + lname;
+// alert("Hello " + fullname)
+// ****************************************** Cheptar # 21 to 25 ***** Task # 2 *****************************
+
+// var mobile = prompt("Enter your favorite Mobile Model")
+
+// var len = mobile.length;
+// alert ("My favorite Mobile model is: " + mobile + " Length of string is: " + len)
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 3 *****************************
+
+// var a = "Pakistan";
+// var b =a.indexOf("n");
+// alert("String: " + a +"\n index of 'n' is: " + b )
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 4 *****************************
+
+
+//  var a = "Hello World";
+//  var b =a.lastIndexOf("l");
+//  alert("String: " + a +"\n index of 'l' is: " + b )
+// ****************************************** Cheptar # 21 to 25 ***** Task # 5 *****************************
+
+//  var a = "Pakistan";
+//  var b =a.charAt(3);
+//  alert("String: " + a +"\n Charaterr at index 3 : " + b )
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 6 *****************************
+
+//  var a = "Pakistan";
+//  b = " Zidabad"
+//  var c =a.concat(b);
+//   alert(c)
+// ****************************************** Cheptar # 21 to 25 ***** Task # 7 *****************************
+
+// var a = "Hyderabad";
+// var b = a.replace("Hyder","Islam");
+// alert(b)
+// ****************************************** Cheptar # 21 to 25 ***** Task # 8 *****************************
+
+// var a = "Ali and Sami are best friends. They play cricket and football together";
+// var b = a.replace(/and/g, "&")
+// alert(b)
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 9 *****************************
+
+// var a = "472";
+// document.write("Value: " + a + "<br>")
+// document.write("Type of: " +  typeof(a) + "<br>")
+// document.write("Value: " + a + "<br>")
+// var b = parseInt(a)
+// document.write("Type of: " +  typeof(b) + "<br>")
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 10 *****************************
+
+// var a = prompt("Enter something");
+// a = a.toUpperCase();
+// document.write(a)
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 11 *****************************
+
+//  var a = prompt("Enter something");
+//  b = a.slice(0,1)
+//  b = b.toUpperCase()
+//  c = a.slice(1)
+//  c = c.toLowerCase()
+//  document.write(b+c)
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 12 *****************************
+
+
+//  var a = 35.36
+//  document.write(typeof(a)+ " : " + a +"<br>")
+//  a = a.toString()
+//  var b = a.replace(".", "")
+//   document.write("Result : " + b)
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 13 *****************************
+
+// var username = prompt("Enter user name")
+// let a = String.fromCharCode(33);
+// let b = String.fromCharCode(44);
+// let c = String.fromCharCode(46);
+// let d = String.fromCharCode(64);
+// for(var i = 0; i < username.length; i++){
+// if(username[i] === a || username[i] === b || username[i] === c || username[i] === d){
+//     alert("please enter correct name")
+// }
+
+// }
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 14 *****************************
+
+
+//var a = ["cake", "apple pie", "cookie", "chips", "patties"];
+//  var search = prompt("Welcom to 3b Onlie shop what do you want sir/ma'am");
+// search = search.toLocaleLowerCase();
+
+// if(a.indexOf(search)!= -1 ){
+//      document.write("Yes " + search + " is avalible index of " + a.indexOf(search))
+//  }
+//  else{
+//      document.write("No " + search + " is not avalible ")
+
+//  }
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 15 *****************************
+
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 16 *****************************
+
+// var university = "University of Karachi";
+
+// for(var i = 0; i < university.length; i++){
+
+//     document.write(university[i] + "<br>")
+// }
+
+// ****************************************** Cheptar # 21 to 25 ***** Task # 17 *****************************
+
+// var a = prompt("Enter something")
+// b = a.charAt(a.length-1);
+// alert(b)
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 1 *****************************
+
+
+// var a = +prompt("Enter positive integer")
+
+// document.write(typeof(a) + " : " + a +"<br>")
+// document.write("rounded of value:" + Math.round(a) + "<br>")
+// document.write("floor value :" + Math.floor(a) + "<br>")
+// document.write("Cei value :" + Math.ceil(a) + "<br>")
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 2 *****************************
+
+
+//  var a = +prompt("Enter Nagitive integer")
+
+//  document.write(typeof(a) + " : " + a +"<br>")
+//  document.write("rounded of value:" + Math.round(a) + "<br>")
+// document.write("floor value :" + Math.floor(a) + "<br>")
+// document.write("Cei value :" + Math.ceil(a) + "<br>")
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 3 *****************************
+
+// var a = -4;
+// document.write("Absolute value of " + a + " is " + Math.abs(a))
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 4*****************************
+
+// var num = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice valuev: "+ num  + "<br>")
+// var num = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice valuev: "+ num  + "<br>")
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 5*****************************
+
+// var head = Math.floor(Math.random() * 2) + 1;
+// if (head === 1){
+// document.write("Random coin valuev Heads : <br>")
+// }
+
+// else{
+
+//     document.write("Random coin valuev Tails : <br>")
+
+// }
+// var head = Math.floor(Math.random() * 2) + 1;
+// if (head === 1){
+//     document.write("Random coin valuev Heads : <br>")
+//     }
+    
+//     else{
+    
+//         document.write("Random coin valuev Tails : <br>")
+    
+//     }
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 6 *****************************
+
+// var num = Math.floor(Math.random() * 100) + 1;
+// document.write("Random Numbers between 1 to 100 : "+ num  + "<br>")
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 7 *****************************
+
+
+// var a = prompt("Enter your weight")
+
+// document.write("The weight of user is " + parseFloat(a) + " kilograms")
+
+
+// ****************************************** Cheptar # 26 to 30 ***** Task # 7 *****************************
+// var num = Math.floor(Math.random() * 10) + 1;
+
+// var num1 = +prompt("Enter a Number between 1 to 1o")
+
+// if(num1 === num){
+//     alert("Congratulate your name is match")
+// }
+
+// else{
+
+//     alert("Try Again!")
+
+// }
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 1 *****************************
+
+// var date = new Date()
+// document.write(date)
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 2 *****************************
+
+
+// var month =  ['January','February','March','April','May','June','July','August','September','October','November','December']
+// var d = new Date()
+
+// alert("Month : " + month[d.getMonth()])
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 3 *****************************
+
+
+//  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var curr = new Date();
+//  var currday = curr.getDay();
+//  var nameOfToday = days[currday];
+//  alert("Today is " + nameOfToday)
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 4 *****************************
+
+//   var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//   var now = new Date();
+//   var theDay = now.getDay();
+//   var nameOfToday = dayNames[theDay];
+//    if(nameOfToday === "Sun" || nameOfToday === "Sat"){
+//       alert("It is Fun Day!")
+//    }
+//   else{
+//     alert("Today is " + nameOfToday)
+// }
+// ****************************************** Cheptar # 31 to 34 ***** Task # 5 *****************************
+
+    //    var now = new Date();
+    //    var theDate = now.getDate();
+    //    if(theDate < 16 ){
+    //        alert("First fiteen days of the Month")
+
+    //    }
+    //    else{
+    //     alert("last days of the Month")
+
+    //    }
+
+    // ****************************************** Cheptar # 31 to 34 ***** Task # 6 *****************************
+
+    // var now = new Date();
+    // var milisec = now.getTime();
+    // var min = milisec/60
+    // document.write(" Current Date: "+ now + "<br>")
+    // document.write(" Elaps Miliseconds since 1 jan 1970: " +milisec + "<br>")
+    // document.write(" Elaps Minutes since 1 jan 1970: " +min)
+
+ // ****************************************** Cheptar # 31 to 34 ***** Task # 7 *****************************
+
+//  var now = new Date()
+//  var currTime = now.getHours()
+//  document.write(currTime)
+
+//  if(currTime < 13 ){
+//      document.write("It is AM")
+//  }
+//  else{
+//     document.write("It is PM")
+//  }
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 8 *****************************
+
+// var now = new Date("December,31,2020")
+// document.write("Later Date: "+ now)
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 9 *****************************
+// var nowTime = new Date()
+// var nowmili = nowTime.getTime()
+// var now = new Date("June,18,2015")
+// var currDay = now.getTime()
+
+// var diff = nowmili - currDay
+// var nowday = diff/(1000 * 60 * 60 * 24);
+// var nowday = Math.floor(nowday)
+// document.write(nowday + " Days passed since first Ramazan 2015")
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 10 *****************************
+
+// var nowTime = new Date()
+// var nowmili = nowTime.getTime()
+// var now = new Date("June 18,2015 22:50:16")
+// var currDay = now.getTime()
+// var diff = nowmili - currDay
+// var nowday = diff/(1000 );
+// var nowday = Math.floor(nowday)
+// document.write(" on Reference date " + now + "<br>")
+// document.write(nowday + "Seconds has passed since beginning of 2015")
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 11 *****************************
+
+// var now = new Date()
+// document.write("Current Date: "+ now + "<br>")
+// var paseYear=now.setHours(now.getHours()-1)
+// document.write("1 hour ago it was " +now)
+
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 12 ***********************
+
+// var now = new Date()
+// document.write("Current Date: "+ now + "<br>")
+// var pastYear= now.setFullYear(now.getFullYear()-100)
+// document.write("100 years back it was " +now)
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 12 ***********************
+
+// var user = prompt("Enter Date of Birth","month day years");
+// var dob = new Date(user)
+// var birthyear = dob.getFullYear()
+// var dobmili = dob.getTime()
+// var now = new Date()
+// var nowmili = now.getTime()
+// var diff = nowmili - dobmili
+// var acc = Math.floor(diff/(1000*60*60*24*30*12))
+// document.write("You are " + acc + " Old <br>" )
+// document.write("You brith year is " +  birthyear )
+
+// ****************************************** Cheptar # 31 to 34 ***** Task # 13 ***********************
+
+// var month =  ['January','February','March','April','May','June','July','August','September','October','November','December']
+// var userName = "Nikshad Ali"
+// var currMonth = month[ new Date().getMonth()]
+// var Units = 410
+// var chargePerUnit = 16;
+// var lateSurcharge = 350
+
+// document.write("<h1>K-Electrical Bill</h1> <br>")
+// document.write("Customer Name: " + userName + "<br>")
+// document.write("Month : " + currMonth + "<br>")
+// document.write("Number of Units: " + Units + "<br>")
+// document.write("Charge per Unit: " + chargePerUnit + "<br>")
+
+// document.write("Net Amount Payable (within due Date): " + Units * chargePerUnit + "<br>")
+// document.write("Late Payment Surcharge : " + lateSurcharge + "<br>")
+// document.write("Gross Amount Payable (After due Date) : " + ( Units * chargePerUnit + lateSurcharge) + "<br>")
+
+
+
+
+
+
+
+
